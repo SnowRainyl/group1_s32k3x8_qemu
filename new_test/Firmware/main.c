3cb76545515da55519b6c8a012b15abebc29b80d
Burico Rebecca <s338247@studenti.polito.it>
2025-04-29 15:58:26 +0000

    moddify main.c

--- new_test/Firmware/main.c
+++ new_test/Firmware/main.c
@@ -2,9 +2,11 @@
 #include <stdint.h>
 #include "FreeRTOS.h"
 #include "task.h"
+#include "portmacro.h" 
 
 /* UART base address */
-#define UART_BASE 0x40000000U
+#define S32K3_PERIPH_BASE        0x40000000
+#define S32K3_UART_BASE          (S32K3_PERIPH_BASE + 0x4A000) 
 
 /* UART register offsets */
 #define UART_GLOBAL_OFFSET   0x08
@@ -18,7 +20,7 @@
 #define CTRL_RE      (1 << 18)
 
 /* Access macros */
-#define UART_REG(offset) (*(volatile uint32_t *)(UART_BASE + (offset)))
+#define UART_REG(offset) (*(volatile uint32_t *)(S32K3_UART_BASE + (offset)))
 
 /* UART driver functions */
 static void uart_init(void)
@@ -46,9 +48,11 @@ static void uart_send_string(const char *str)
     }
 }
 
+
 /* FreeRTOS task */
 void TxTask(void *parameters)
 {
+    (void)parameters;	
     for(;;) {
         uart_send_string("Hello from FreeRTOS running in QEMU!\r\n");
         vTaskDelay(pdMS_TO_TICKS(1000));  // Delay 1 second
@@ -58,13 +62,18 @@ void TxTask(void *parameters)
 /* main function */
 int main(void)
 {
+    
     uart_init();
+    uart_send_string(">>> main() started\n");
 
-    xTaskCreate(TxTask, "TxTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xTaskCreate(TxTask, "TxTask", configMINIMAL_STACK_SIZE, NULL, mainTASK_PRIORITY, NULL);
+    
 
+    uart_send_string("Starting scheduler...\n");
     vTaskStartScheduler();
 
-    // Should never reach here
-    while (1) {}
+    uart_send_string("Scheduler returned (should never happen)\n");
+    return 1;// Should never reach here
+    
+    //while (1) {}
 }
-
