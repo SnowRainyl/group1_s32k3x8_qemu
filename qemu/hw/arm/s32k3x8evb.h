73bcfeae399466ba754563bcadc592eac2404806
Burico Rebecca <s338247@studenti.polito.it>
2025-06-26 16:45:57 +0000

    Update file s32k3x8evb.h

--- qemu/hw/arm/s32k3x8evb.h
+++ qemu/hw/arm/s32k3x8evb.h
@@ -9,27 +9,28 @@
 #include "hw/arm/armv7m.h"
 #include "hw/sysbus.h"
 
-// define the size of memory
-#define S32K3X8EVB_FLASH_SIZE (4 * MiB)  
+// Define memory sizes
+#define S32K3X8EVB_FLASH_SIZE (4 * MiB)
 #define S32K3X8EVB_SRAM_SIZE  (1 * MiB)
-//define the size of memory--mapping
-#define S32K3_FLASH_BASE 0x00400000  //Flash base
-#define S32K3_SRAM0_BASE 0x20400000
 
-// 定义设备类型标识符
-#define TYPE_S32K3X8EVB        TYPE_S32K3X8EVB_MACHINE
+// Define memory-mapped base addresses
+#define S32K3_FLASH_BASE 0x00400000  // Flash base address
+#define S32K3_SRAM0_BASE 0x20400000  // SRAM base address
+
+// Define device type identifiers
+#define TYPE_S32K3X8EVB         TYPE_S32K3X8EVB_MACHINE
 #define TYPE_S32K3X8EVB_MACHINE "s32k3x8evb-machine"
-// ROM 设备定义
+
+// ROM device type definition
 #define TYPE_S32K3X8_ROM "s32k3x8.rom"
 #define VECTOR_TABLE_BASE S32K3_FLASH_BASE
-
 
 typedef struct S32K3X8ROMState {
     SysBusDevice parent_obj;
     MemoryRegion flash;
 } S32K3X8ROMState;
 
-// RAM 设备定义
+// RAM device type definition
 #define TYPE_S32K3X8_RAM "s32k3x8.ram"
 
 typedef struct S32K3X8RAMState {
@@ -37,26 +38,23 @@ typedef struct S32K3X8RAMState {
     MemoryRegion sram;
 } S32K3X8RAMState;
 
-
+// Number of LPSPI instances
 #define S32K3X8_LPSPI_COUNT 6
 
-// 板级状态结构体 - 添加 LPSPI 支持
+// Board state structure - includes support for LPSPI
 typedef struct S32K3X8EVBState {
     MachineState parent_obj;
     DeviceState *rom;
     DeviceState *ram;
     ARMv7MState armv7m;
     DeviceState *uart;
-    DeviceState *spi;
-    
-    /* 新增：LPSPI 设备数组 */
-    DeviceState *lpspi[S32K3X8_LPSPI_COUNT];  /* LPSPI0-LPSPI5 */
-    
+    /* Added: array of LPSPI device instances (LPSPI0 to LPSPI5) */
+    DeviceState *lpspi[S32K3X8_LPSPI_COUNT];
 } S32K3X8EVBState;
 
-// 声明状态检查宏
-DECLARE_INSTANCE_CHECKER(S32K3X8EVBState, S32K3X8EVB_MACHINE, 
-                        TYPE_S32K3X8EVB_MACHINE)
+// Declare instance checker macros
+DECLARE_INSTANCE_CHECKER(S32K3X8EVBState, S32K3X8EVB_MACHINE,
+                         TYPE_S32K3X8EVB_MACHINE)
 DECLARE_INSTANCE_CHECKER(S32K3X8ROMState, S32K3X8_ROM, TYPE_S32K3X8_ROM)
 DECLARE_INSTANCE_CHECKER(S32K3X8RAMState, S32K3X8_RAM, TYPE_S32K3X8_RAM)
 
